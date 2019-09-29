5fc22fb72267f8e103af42f9382947175628828f
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-29 13:22:19 +0700

    adding connection to database

--- WindowsFormsApp1/FormLogin.cs
+++ WindowsFormsApp1/FormLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class FormLogin : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-N2LO0FB;Initial Catalog=ToughputTest2;Integrated Security=True");
         public FormLogin()
         {
             InitializeComponent();
